f6d056df9a7e30a325560724debf7540f66b431b
davydova <natalya-davydova-86@mail.ru>
2023-04-25 18:37:52 +0300

    Added folder Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача №3
+//напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Wodnesday
+//5 -> Friday
+
+string a = Console.ReadLine();
+if (a == "1") Console.WriteLine("Monday");
+else if (a == "2") Console.WriteLine("Tusday");
+else if (a == "3") Console.WriteLine("Wednesday");
+else if (a == "4") Console.WriteLine("Thursday");
+else if (a == "5") Console.WriteLine("Friday");
+else if (a == "6") Console.WriteLine("Saturday");
+else if (a == "7") Console.WriteLine("Sunday");
+else Console.WriteLine("Incorrect value");
