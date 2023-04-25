341f6fcaab70dcfb40cfb7ef1c71fc9c70f01bbd
davydova <natalya-davydova-86@mail.ru>
2023-04-25 17:12:31 +0300

    Added folder Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача №1
+//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> Yes
+//a = 2, b = 10 -> No
+//a = 9, b = -3 -> Yes
+//a = -3, b = 9 -> No
+
+Console.WriteLine("Введите число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == a)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
