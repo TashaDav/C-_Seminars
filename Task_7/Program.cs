2bb2a94607bf1bddac4c36875db56aa750b7ba36
davydova <natalya-davydova-86@mail.ru>
2023-04-25 23:17:41 +0300

    Added Task 7

---
+++ Task_7/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача №7
+// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает полсденюю цифру этого числа.
+
+// string input = Console.ReadLine();
+// int str_length = input.Length;
+// Console.WriteLine(input[str_length - 1]);
+
+//Можно записать короче
+
+// string a = Console.ReadLine();
+// int xa = Convert.ToInt32(a);
+// int b = xa %10;
+// Console.WriteLine(b);
+
+//Ещё короче
+// Console.WriteLine(Console.ReadLine().Last());
+
+Console.Write("Введите трехзначное число ");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a > 99 & a < 1000) {
+Console.WriteLine(a % 10);
+}
+else
+{
+Console.WriteLine("Вы ввели не трехзначное число");
+}
+
