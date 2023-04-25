16de42588ffc899c9854e30ed99cdcf2bb5646b8
davydova <natalya-davydova-86@mail.ru>
2023-04-25 20:25:56 +0300

    Added case switch

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -3,12 +3,44 @@
 //3 -> Wodnesday
 //5 -> Friday
 
-string a = Console.ReadLine();
-if (a == "1") Console.WriteLine("Monday");
-else if (a == "2") Console.WriteLine("Tusday");
-else if (a == "3") Console.WriteLine("Wednesday");
-else if (a == "4") Console.WriteLine("Thursday");
-else if (a == "5") Console.WriteLine("Friday");
-else if (a == "6") Console.WriteLine("Saturday");
-else if (a == "7") Console.WriteLine("Sunday");
-else Console.WriteLine("Incorrect value");
+// string a = Console.ReadLine();
+// if (a == "1") Console.WriteLine("Monday");
+// else if (a == "2") Console.WriteLine("Tusday");
+// else if (a == "3") Console.WriteLine("Wednesday");
+// else if (a == "4") Console.WriteLine("Thursday");
+// else if (a == "5") Console.WriteLine("Friday");
+// else if (a == "6") Console.WriteLine("Saturday");
+// else if (a == "7") Console.WriteLine("Sunday");
+// else Console.WriteLine("Incorrect value");
+
+// Для решения лучше использовать оператор SWITCH
+
+Console.WriteLine("Введите номер дня недели");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("Нет такого номера дня недели");
+        break;
+}
