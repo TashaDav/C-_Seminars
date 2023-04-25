6bea302b3d0a696b76020e9b9726513dced48d3f
davydova <natalya-davydova-86@mail.ru>
2023-04-25 16:29:33 +0300

    Added Pracitce Seminar

---
+++ Pracitce_Seminar№1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Здача №1
+// Напишите программу, которая на выход принимает число и выдает его квадрат (число умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+//1.Принимать число
+//2.Посчитать квадрат
+//3.Вывести число
+
+//string b = Console.ReadLine();
+//int a =Convert.ToInt32(b); // Convert.ToInt32(Console.ReadLine()) = Convert.ToInt32(b). 
+//int result = a*a;
+//Console.WriteLine(result);
+
+// more short write:
+//int a = Convert.ToInt32(Console.ReadLine());
+// int result = a * a;
+//Console.WriteLine(a * a);
+
+//ИТЕРАЦИЯ №1. РАБОТА В ГРУППАХ
+
+
+//Задача №3.
+//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
